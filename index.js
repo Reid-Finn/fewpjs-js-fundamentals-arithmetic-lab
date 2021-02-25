d17805449d5b3f71493f4ba3c847aceef2fedb63
Reid <Reid.Finn@yahoo.com>
2021-02-25 14:57:25 -0600

    commit tree 993b3be3b5e81a724da041cd063f433cea9e2daf

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+//2
+let random = Math.random();
+//3
+let mod = 16 % 6;
+//4
+let max = Math.max(1, 6, 18, 20);
